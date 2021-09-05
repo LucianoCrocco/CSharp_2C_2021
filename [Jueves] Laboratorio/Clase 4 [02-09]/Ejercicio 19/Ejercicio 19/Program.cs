15c6e360d31bfc7833cf479e3b80eed6ae7608e5
LucianoCrocco <lucianoacrocco@gmail.com>
2021-09-05 18:59:29 -0300

    Ejercicio 19

--- [Jueves] Laboratorio/Clase 4 [02-09]/Ejercicio 19/Ejercicio 19/Program.cs
+++ [Jueves] Laboratorio/Clase 4 [02-09]/Ejercicio 19/Ejercicio 19/Program.cs
@@ -7,11 +7,16 @@ namespace Ejercicio_19
     {
         static void Main(string[] args)
         {
-            Sumador s1 = new Sumador();
+            Sumador s1 = new Sumador(10);
             Sumador s2 = new Sumador(10);
+            Sumador s3 = new Sumador();
 
-            
+            Console.WriteLine(s1 | s2);
+            Console.WriteLine(s1 + s2);
 
+            Console.WriteLine(s3.Sumar((int)s1, 30));
+            Console.WriteLine(s1 | s3);
+            Console.WriteLine(s1 + s3);
 
         }
     }
--- [Jueves] Laboratorio/Clase 4 [02-09]/Ejercicio 19/Entidades/Sumador.cs
+++ [Jueves] Laboratorio/Clase 4 [02-09]/Ejercicio 19/Entidades/Sumador.cs
@@ -6,13 +6,15 @@ namespace Entidades
     {
         private int cantidadDeSumas;
 
-        public Sumador() :
-            this(0)
-        { }
         public Sumador(int inicializacion)
         {
             this.cantidadDeSumas = inicializacion;
         }
+
+        public Sumador(): 
+            this(0)
+        { }
+
 
         public long Sumar(long a, long b)
         {
@@ -26,12 +28,24 @@ namespace Entidades
             return String.Concat(a, b);
         }
 
+
         public static explicit operator int (Sumador x) {
             return x.cantidadDeSumas;
         }
 
-        public static long operator +()
+        public static long operator +(Sumador s1, Sumador s2)
+        {
+            return s1.cantidadDeSumas + s2.cantidadDeSumas;
+        }
 
+        public static bool operator |(Sumador s1, Sumador s2)
+        {
+            if(s1.cantidadDeSumas == s2.cantidadDeSumas)
+            {
+                return true;
+            }
+            return false;
+        }
 
     }
 }
