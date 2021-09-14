048706df76ca6520c62544b1e7f4c67ddeeef4aa
LucianoCrocco <lucianoacrocco@gmail.com>
2021-09-14 19:11:49 -0300

    Guia al dia

---
+++ [Jueves] Laboratorio/Clase 6 [09-09]/Ejercicio 28/Ejercicio 28/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace Ejercicio_28
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextPalabras = new System.Windows.Forms.RichTextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextPalabras
+            // 
+            this.richTextPalabras.Location = new System.Drawing.Point(12, 12);
+            this.richTextPalabras.Name = "richTextPalabras";
+            this.richTextPalabras.Size = new System.Drawing.Size(373, 284);
+            this.richTextPalabras.TabIndex = 0;
+            this.richTextPalabras.Text = "";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(310, 302);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(397, 335);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.richTextPalabras);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextPalabras;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ [Jueves] Laboratorio/Clase 6 [09-09]/Ejercicio 28/Ejercicio 28/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_28
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(richTextPalabras.Text))
+            {
+                Dictionary<string, int> diccionarioPalabras = new Dictionary<string, int>();
+                List<string> topPalabaras = new List<string>();
+
+                string palabras = richTextPalabras.Text;
+                string[] palabrasArray;
+                palabras = palabras.ToLower();
+                palabrasArray = palabras.Split('\n');
+
+                StringBuilder sb = new StringBuilder();
+
+                int contador = 0;
+                int segundoContador = 0;
+                int tercerContador = 0;
+
+                foreach (string palabra in palabrasArray)
+                {
+                    if (!diccionarioPalabras.ContainsKey(palabra))
+                    {
+                        diccionarioPalabras.Add(palabra, 1);
+                    } else
+                    {
+                        diccionarioPalabras[palabra] += 1;
+                    }
+                }
+
+                foreach (KeyValuePair<string, int> palabra in diccionarioPalabras)
+                {
+                    if (palabra.Value > contador)
+                    {
+                        contador = palabra.Value;
+                        topPalabaras.Insert(0, palabra.Key);
+                    }
+                    else if (palabra.Value > segundoContador)
+                    {
+                        segundoContador = palabra.Value;
+                        segundoContador++;
+                        topPalabaras.Insert(1, palabra.Key);
+                    }
+                    else if (palabra.Value > tercerContador)
+                    {
+                        tercerContador = palabra.Value;
+                        tercerContador++;
+                        topPalabaras.Insert(2, palabra.Key);
+                    }
+                }
+
+                for(int i = 0; i < 3; i++)
+                {
+                    sb.AppendLine(topPalabaras[i]);
+                }
+                richTextPalabras.Text = "\tLa lista de palabras mas utlizadas\n" + sb.ToString();
+            } else
+            {
+                MessageBox.Show("Ingrese palabras para calcular", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+    }
+}
