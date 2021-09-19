ee247a34d2b69b716790c8ea1f9d20cd37f344dd
LucianoCrocco <lucianoacrocco@gmail.com>
2021-09-19 03:07:19 -0300

    .

---
+++ Ejercicios Extra/Ejercicio Extra (C5)/Entidades/Estante.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Estante
+    {
+        #region Atributos
+        private Producto[] productos;
+        private int ubicacionEstante;
+        #endregion
+
+        #region Constructores
+        private Estante(int capacidad)
+        {
+            this.productos = new Producto[capacidad];
+        }
+
+        public Estante(int capacidad, int ubicacion)
+            : this(capacidad)
+        {
+            this.ubicacionEstante = ubicacion;
+        }
+        #endregion
+
+        #region Getters y Setters
+        public Producto[] Productos{
+            get
+            {
+                return this.productos;
+            }
+        }
+        #endregion
+
+        #region Metodos
+        public static string MostrarEstante (Estante e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("UBICACION ESTANTE: " + e.ubicacionEstante);
+            sb.AppendLine("PRODUCTOS EN EL ESTANTE: ");
+            foreach(Producto producto in e.Productos)
+            {
+                sb.AppendLine(Producto.MostrarProducto(producto));
+            }
+            return sb.ToString();
+        }
+        #endregion
+
+        #region Sobrecarga de Operadores
+        public static bool operator ==(Estante e, Producto p)
+        {
+            foreach(Producto producto in e.Productos)
+            {
+                if(producto == p)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool operator !=(Estante e, Producto p)
+        {
+            return !(e == p);
+        }
+
+        public static bool operator +(Estante e, Producto p)
+        {
+            if(e != p){
+                for(int i = 0; i < e.Productos.Length; i++)
+                {
+                    if (e.Productos[i] is null)
+                    {
+                        e.productos[i] = p;
+                        return true; ;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static bool operator -(Estante e, Producto p)
+        {
+            if(e == p)
+            {
+                for(int i = 0; i < e.Productos.Length; i++)
+                {
+                    if(e.productos[i] == p)
+                    {
+                        e.productos[i] = null;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        #endregion
+    }
+}
---
+++ Ejercicios Extra/Ejercicio Extra (C5)/Entidades/Producto.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Text;
+
+namespace Entidades
+{
+    public class Producto
+    {
+        #region Atributos
+        private string codigoDeBarras;
+        private string marca;
+        private float precio;
+        #endregion
+
+        #region Constructores
+        public Producto(string marca, string codigo, float precio)
+        {
+            this.marca = marca;
+            this.codigoDeBarras = codigo;
+            this.precio = precio;
+        }
+        #endregion
+
+        #region Getters y Setters
+        public string Marca
+        {
+            get
+            {
+                return this.marca;
+            }
+        }
+        public float Precio
+        {
+            get
+            {
+                return this.precio;
+            }
+        }
+        #endregion
+
+        #region Metodos
+        public static string MostrarProducto(Producto p)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("DATOS PRODUCTO: ");
+            sb.AppendLine("Marca: " + p.Marca);
+            sb.AppendLine("Precio: " + p.Precio);
+
+            return sb.ToString();
+        }
+        #endregion
+
+        #region Sobrecarga Explicita
+        public static explicit operator string(Producto p)
+        {
+            string retorno = p.codigoDeBarras;
+            return retorno;
+        }
+        #endregion
+
+        #region Sobrecarga de Operadores
+        public static bool operator ==(Producto p, string marca){
+            if(p.Marca == marca)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool operator !=(Producto p, string marca)
+        {
+            return !(p == marca);
+        }
+
+        public static bool operator ==(Producto p1, Producto p2)
+        {
+            if (p1.Marca == p2.Marca && ((string)p1) == ((string)p2) && !(p1 is null) && !(p2 is null))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            return !(p1 == p2);
+        }
+        #endregion
+    }
+}
