1554a7d9f9c5fe9f9e4fb5df8b56337e8539a417
LucianoCrocco <lucianoacrocco@gmail.com>
2021-09-21 02:12:50 -0300

    Ejercicios Herencia terminado

---
+++ [Jueves] Laboratorio/Clase 8 [16-09]/CentralTelefonica/CentralitaHerencia/Centralita.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+    public class Centralita
+    {
+        #region Atributos
+        private List<Llamada> listaDeLlamadas;
+        protected string razonSocial;
+        #endregion
+
+        #region Constructores
+        public Centralita()
+        {
+            listaDeLlamadas = new List<Llamada>();
+        }
+
+        public Centralita(string nombreEmpresa)
+            : this()
+        {
+            this.razonSocial = nombreEmpresa;
+        }
+        #endregion
+
+        #region Getters y Setters
+        public float GananciasPorLocal
+        {
+            get
+            {
+                return this.CalcularGanancia(Llamada.TipoLlamada.Local);
+            }
+        }
+
+        public float GananciasPorProvincial
+        {
+            get
+            {
+                return this.CalcularGanancia(Llamada.TipoLlamada.Provincial);
+            }
+        }
+
+        public float GananciasPorTotal
+        {
+            get
+            {
+                return this.CalcularGanancia(Llamada.TipoLlamada.Todas);
+            }
+        }
+
+        public List<Llamada> Llamadas
+        {
+            get
+            {
+                return this.listaDeLlamadas;
+            }
+        }
+
+        #endregion
+
+        #region Metodos
+        private float CalcularGanancia(Llamada.TipoLlamada tipo)
+        {
+            float retorno = 0;
+            foreach(Llamada llamada in this.Llamadas)
+            {
+                switch (tipo)
+                {
+                    case Llamada.TipoLlamada.Local:
+                        if (llamada is Local)
+                        {
+                            retorno += ((Local)llamada).CostoLlamada;
+                        }
+                        break;
+                    case Llamada.TipoLlamada.Provincial:
+                        if (llamada is Provincial)
+                        {
+                            retorno += ((Provincial)llamada).CostoLlamada;
+                        }
+                        break;
+                    default:
+                        if (llamada is Provincial)
+                        {
+                            retorno += ((Provincial)llamada).CostoLlamada;
+                        } else if(llamada is Local)
+                        {
+                            retorno += ((Local)llamada).CostoLlamada;
+                        }
+                        break;
+                }
+            }
+            return retorno;
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("\tDATOS LLAMADAS A CENTRALITA");
+            sb.AppendLine("Razon Social: " + this.razonSocial);
+            sb.AppendLine("Ganancia por Total" + this.CalcularGanancia(Llamada.TipoLlamada.Todas));
+            sb.AppendLine("Ganancia por Local" + this.CalcularGanancia(Llamada.TipoLlamada.Local));
+            sb.AppendLine("Ganancia por Provincial" + this.CalcularGanancia(Llamada.TipoLlamada.Provincial));
+            sb.AppendLine("\tLISTA DE LLAMADOS");
+            foreach(Llamada llamada in this.Llamadas)
+            {
+                sb.AppendLine(llamada.Mostrar());
+            }
+
+            return sb.ToString();
+        }
+
+        public void OrdenarLlamadas()
+        {
+            this.listaDeLlamadas.Sort(Llamada.OrdenarPorDuracion);
+        }
+        #endregion
+    }
+}
---
+++ [Jueves] Laboratorio/Clase 8 [16-09]/CentralTelefonica/CentralitaHerencia/Provincial.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+    public class Provincial : Llamada
+    {
+
+        #region Enumerados
+        public enum Franja{
+            Franja_1,
+            Franja_2,
+            Franja_3
+        }
+        #endregion
+
+        #region Atributos
+        protected Franja franja;
+        #endregion
+
+        #region Constructores
+        public Provincial(Franja miFranja, Llamada llamada)
+            : base(llamada.Duracion, llamada.NroDestino, llamada.NroOrigen)
+        {
+            this.franja = miFranja;   
+        }
+
+        public Provincial(string origen, Franja miFranja, float duracion, string destino)
+            : base(duracion, destino, origen)
+        {
+            this.franja = miFranja;
+        }
+        #endregion
+
+        #region Setters y Getters
+        public float CostoLlamada
+        {
+            get
+            {
+                return this.CalcularCosto();
+            }
+        }
+        #endregion
+
+        #region Metodos
+        private float CalcularCosto()
+        {
+            float precio;
+            switch (this.franja)
+            {
+                case Franja.Franja_1:
+                    precio = (float) 0.99;
+                    break;
+                case Franja.Franja_2:
+                    precio = (float) 1.25;
+                    break;
+                default:
+                    precio = (float) 0.66;
+                    break;
+            }
+
+            return this.Duracion * precio;
+        }
+
+        public new string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(base.Mostrar());
+            sb.AppendLine("\t DATOS DE LLAMADA PROVINCIAL: ");
+            sb.AppendLine("Coste: " + this.CostoLlamada);
+            sb.AppendLine("Franja de llamado: " + this.franja);
+
+            return sb.ToString();
+        }
+        #endregion
+
+    }
+}
